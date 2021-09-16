4bd9fc486ba867840afc35508a3027f2d21f3ae6
AhmadSupriyatna <ahmadsupriyatna1@gmail.com>
2021-09-16 14:14:38 +0700

    Halaman Menu Utama
    Tugas pada halaman menu Utama

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Header from "./Page/Header";
+import Footer from "./Page/Footer";
+import MenuUtama from "./Page/MenuUtama";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <MenuUtama />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Page/MenuMakanan.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class MenuMakanan extends Component {
+    render() {
+        return (
+            <div>
+                <listMenuUtama />
+            </div>
+        )
+    }
+}
+
+export default MenuMakanan
