583727701812895c9ee5271a2ab76c5d9e14a8af
AhmadSupriyatna <ahmadsupriyatna1@gmail.com>
2021-09-16 15:01:06 +0700

    Tugas 2 React Js
    Tugas ini kami kumpulkan Untuk Niomic

--- src/Page/MenuTentangKami.js
+++ src/Page/MenuTentangKami.js
@@ -2,12 +2,15 @@ import React, { Component } from 'react'
 
 export class MenuTentangKami extends Component {
     constructor (props){
-        super (props)
+        super (props);
+        this.state = {
+            tentang: "Warung Nusantara adalah Restoranyang bernuansa Msakan Khas Nusantara, Kami ada untuk membuat adna menkmatai masakan khas Nusantara",
+        };
     }
     render() {
         return (
-            <div>
-                
+            <div align="center">
+                <p>Tentang Kami</p>{this.state.tentang}
             </div>
         )
     }
